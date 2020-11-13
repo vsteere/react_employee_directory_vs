7eb74228b7d7abc55c78e72221a8b58cafb559c6
vsteere <vssteere@gmail.com>
2020-11-12 19:20:18 -0600

    work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import EmployeeTable from "./components/EmployeeTable"
 
 function App() {
   const [employees, setEmployees] = useState([]);
-	useEffect(() => {
-		axios
-			.get("https://randomuser.me/api/?results=10&nat=us")
-			.then((response) => {
-				setEmployees([...response.data.results]);
-				//localStorage.setItem('employees_ls', JSON.stringify([...response.data.results]));
-				console.log(employees);
-			});
-	}, []);
-  
-  
+  useEffect(() => {
+    axios
+      .get("https://randomuser.me/api/?results=10&nat=us")
+      .then((response) => {
+        setEmployees([...response.data.results]);
+        console.log(response);
+      });
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <EmployeeTable employees = {employees} />
+      
     </div>
   );
 }
---
+++ my-app/src/components/EmployeeTable.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from 'react'
+
+const EmployeeTable = (props) => {
+console.log(props);
+
+    return (
+        <>
+         <table>
+             <tr>
+                <th>Name</th>
+                <th>image</th>
+                <th>phone number</th>
+                <th>email</th>
+             </tr>
+             
+             
+             
+             
+             </table>   
+        </>
+    )
+}
+
+export default EmployeeTable
